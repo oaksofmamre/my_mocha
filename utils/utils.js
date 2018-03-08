0cecdedc5ecb88edfbd46ceb94f9fc8caef9e953
DC <heyyodc-git@yahoo.com>
2018-03-07 17:00:56 -0800

    testing for async

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,26 @@
 "use strict";
 
 const add = (a, b) => a + b;
+
+const asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 1000);
+};
+
 const square = a => a * a;
 
-module.exports = { add, square };
+const asyncSquare = (a, callback) => {
+  setTimeout(() => {
+    callback(a * a);
+  }, 1000);
+};
+
+const setName = (user, fullName) => {
+  let names = fullName.split(" ");
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+};
+
+module.exports = { add, square, setName, asyncAdd, asyncSquare };
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -9,9 +9,15 @@ it("should add 2 numbers", () => {
   expect(result)
     .toBe(7)
     .toBeA("number");
-  // if (result !== 7) {
-  //   throw new Error(`Expected 7 but got back ${result}`);
-  // }
+});
+
+it("should async add 2 numbers", done => {
+  let result = utils.asyncAdd(7, 8, sum => {
+    expect(sum)
+      .toBe(15)
+      .toBeA("number");
+    done();
+  });
 });
 
 it("should square a number", () => {
@@ -19,7 +25,34 @@ it("should square a number", () => {
   expect(result)
     .toBe(9)
     .toBeA("number");
-  // if (result !== 9) {
-  //   throw new Error(`Expected 9 but got back ${result}`);
-  // }
 });
+
+it("should async square a number", done => {
+  let result = utils.asyncSquare(7, square => {
+    expect(square)
+      .toBe(49)
+      .toBeA("number");
+    done();
+  });
+});
+
+it("should set firstname and lastname", () => {
+  let user = { firstName: "", lastName: "", age: 21 };
+  let result = utils.setName(user, "Charlie Brown");
+  expect(result)
+    .toBeAn("object")
+    .toInclude({ firstName: "Charlie", lastName: "Brown" });
+});
+
+// it("should expect some values", () => {
+//   expect(12).toBe(12);
+//   // the below check on object should fail for objects
+//   expect({ word: "ocean" }).toBe({ word: "ocean" });
+//   // thus use toEqual for object comparisons
+//   expect({ word: "ocean" }).toEqual({ word: "ocean" });
+//   // for arrays use toInclude()
+//   expect([1, 2, 3]).toInclude(3);
+//   expect({ name: "dan", age: 21, address: "123 Main St" }).toInclude({
+//     age: 21
+//   });
+// });
